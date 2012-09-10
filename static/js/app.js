79712d4641456455b059c765a07e8bc8b92c3e92
lukin0110 <maarten.huijsmans@gmail.com>
2012-09-11 00:51:43 +0200

    ramp up

--- static/js/app.js
+++ static/js/app.js
@@ -15,3 +15,20 @@ Fx.showLoading = function() {
 Fx.hideLoading = function() {
 
 };
+
+
+
+/**
+ * Hide the navigation bar
+ *
+ * http://davidwalsh.name/hide-address-bar
+ * http://24ways.org/2011/raising-the-bar-on-mobile
+ * http://stackoverflow.com/questions/6011223/how-to-completely-hide-the-navigation-bar-in-iphone-html-5
+ */
+window.addEventListener("load",function() {
+    // Set a timeout...
+    setTimeout(function(){
+        // Hide the address bar!
+        window.scrollTo(0, 0);
+    }, 0);
+});
